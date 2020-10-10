20b6a226c59ca0446cdaa31b448b913003043006
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-10 11:34:30 -0300

    trabalhando para melhorar

--- script.js
+++ script.js
@@ -4,41 +4,33 @@ const btnAdicionar = $('.adicionaLista');
 const ulLista = $('.ulLista');
 
 
-const lista = document.createElement('li');
-const btnFeito = document.createElement('button')
-const btnDeletar = document.createElement('button')
-
-
-btnAdicionar.addEventListener('click', adiciona)
+btnAdicionar.addEventListener('click', adiciona);
 
 function adiciona() {
+    const lista = document.createElement('li');
+    const btnFeito = document.createElement('button');
+    const btnDeletar = document.createElement('button');
 
     lista.innerHTML = `${texto.value}`;
     ulLista.appendChild(lista);
     ulLista.appendChild(btnFeito);
     ulLista.appendChild(btnDeletar);
 
-}
+    btnFeito.innerText = 'Feito';
+    btnFeito.classList.add('btnFeito');
+    btnFeito.addEventListener('click', addClass);
 
+    function addClass() {
 
-btnFeito.innerText = 'Feito';
-btnFeito.classList.add('btnFeito');
-btnFeito.addEventListener('click', addClass);
+        lista.classList.toggle('feito')
+    }
 
-function addClass() {
+    btnDeletar.addEventListener('click', addClassDeletar)
+    btnDeletar.innerText = 'Deletar';
+    btnDeletar.classList.add('btnDeletar');
 
-    lista.classList.toggle('feito')
+    function addClassDeletar() {
 
-}
-
-btnDeletar.addEventListener('click', addClassDeletar)
-
-btnDeletar.innerText = 'Deletar';
-btnDeletar.classList.add('btnDeletar');
-
-function addClassDeletar() {
-
-    lista.remove('li')
-        /* btnDeletar.classList.toggle('removeBtn'),
-         btnFeito.classList.toggle('removeBtn');*/
+        lista.remove(ulLista)
+    }
 }
